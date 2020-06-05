ed7c3bdb0150fd10c722389119741bb8c4d516a7
Jose Ramirez Fuentes <proyectoramirez07@gmail.com>
2020-06-05 16:00:46 -0400

    Revert "Move section template creation to javascript"
    This reverts commit 622eb3de36f37618a24a7c6c09ccdd757e63854b.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -224,24 +224,10 @@ function renderSectionData(sectionData) {
  * @return {!DocumentFragment} The populated section fragment.
  */
 function buildSectionFragment(title, entryData) {
-  const fragment = createSectionFragment();
+  const fragment = document.querySelector('#section').content.cloneNode(/* deep */ true);
   const populatedFragment = populateSectionFragment(fragment, title, entryData);
 
   return populatedFragment;
-}
-
-/**
- * @return {HTMLElement} An empty section fragment
- */
-function createSectionFragment() {
-  const fragment = document.createElement("section");
-
-  const titleContainer = document.createElement("h3");
-  titleContainer.classList.add("title");
-
-  fragment.append(titleContainer);
-
-  return fragment;
 }
 
 /**
